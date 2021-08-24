11fdb8049fca19a9b35ede8f92d35958dab2f587
ashish8796 <ashishsaini.ak@gmail.com>
2021-08-24 12:56:57 +0530

    react app wrapper is done

---
+++ src/Router/Router.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <Switch>
+      <Route path="/" exact></Route>
+    </Switch>
+  );
+}
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./reducer";
+
+const composeEnhancers =
+  (window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+export default store;
