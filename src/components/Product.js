e87c7954b240d19a1438a9f4b3425251099098c5
anish <aanishmv@gmail.com>
2022-09-21 23:16:09 +0530

    added view details functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Product({ items }) {
   const [state, setState] = useState();
+  const Navigate = useNavigate();
+  function handleViewDetails() {
+    Navigate(`/details/${items.productId}`);
+  }
   return (
-    <div className="flex flex-col items-center gap-5 rounded-lg border-2 shadow-md w-96">
+    <div
+      onClick={(e) => handleViewDetails(e)}
+      className="flex flex-col items-center gap-5 rounded-lg border-2 shadow-md w-96"
+    >
       <img
         className=" h-72 w-60 object-contain"
         src={items.imageURL}
